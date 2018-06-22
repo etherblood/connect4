000a3c78a310495be4d4a14232ac135563d6edaf
Etherblood <philipp@jobach.de>
2018-06-23 00:09:05 +0200

    added principal variation search

--- alphabeta.py
+++ alphabeta.py
@@ -4,22 +4,30 @@ from evaluation import evaluate
 from evaluation import LOSS_SCORE
 
 class AlphaBetaBot:
-	def __init__(self, depth=7):
+	def __init__(self, depth=7, pvs_enabled=True):
 		self._depth = depth
+		self._pvs_enabled = pvs_enabled
 		
 	def search(self, state):
 		alpha = LOSS_SCORE - self._depth
 		beta = -alpha
+		foundPV = False
 		moves = state.token_moves()
 		while moves:
 			move = highest_bit(moves)
 			moves ^= move
 			state.token_move(move)
-			score = -self.alphabeta(state, self._depth - 1, -beta, -alpha)
+			if self._pvs_enabled and foundPV:
+				score = -self.alphabeta(state, self._depth - 1, -alpha - 1, -alpha)
+				if alpha < score:
+					score = -self.alphabeta(state, self._depth - 1, -beta, -alpha)
+			else:
+				score = -self.alphabeta(state, self._depth - 1, -beta, -alpha)
 			state.token_undo(move)
 			if score > alpha:
 				alpha = score
 				best_move = move
+				foundPV = True
 		return best_move
 		
 	def alphabeta(self, state, depth, alpha, beta):
@@ -29,17 +37,24 @@ class AlphaBetaBot:
 			return 0
 		if depth <= 0:
 			return evaluate(state)
+		foundPV = False
 		moves = state.token_moves()
 		while moves:
 			move = highest_bit(moves)
 			moves ^= move
 			state.token_move(move)
-			score = -self.alphabeta(state, depth - 1, -beta, -alpha)
+			if self._pvs_enabled and foundPV:
+				score = -self.alphabeta(state, depth - 1, -alpha - 1, -alpha)
+				if alpha < score:
+					score = -self.alphabeta(state, depth - 1, -beta, -alpha)
+			else:
+				score = -self.alphabeta(state, depth - 1, -beta, -alpha)
 			state.token_undo(move)
 			if score > alpha:
 				if score >= beta:
 					alpha = beta
 					break
 				alpha = score
+				foundPV = True
 		return alpha
 	
