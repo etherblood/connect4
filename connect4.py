7c198bf3c133a752287a5dc1fa902a34c727cc1f
Etherblood <philipp@jobach.de>
2018-06-22 02:07:46 +0200

    formatting

--- connect4.py
+++ connect4.py
@@ -1,74 +1,77 @@
-def toFlag(index):
+def to_flag(index):
 	return 1 << index
 	
 class Connect4:
 	
 	def __init__(self, width=7, height=6):
-		self.width = width
-		self.height = height
-		self.playerTokens = [0, 0]
-		self.currentPlayer = 0
+		self._width = width
+		self._height = height
+		self._player_tokens = [0, 0]
+		self._current_player = 0
 		
-		xAxis = toFlag(width) - 1
+		xAxis = to_flag(width) - 1
 		yAxis = 0
 		for y in range(height):
-			yAxis |= toFlag(y * width)
-		self.fullBoard = xAxis * yAxis
-		self.winShifts = [1, width - 1, width + 1, width]
-		self.winMasks = [~yAxis, ~(xAxis | (yAxis << (width - 1))), ~(xAxis | yAxis), ~xAxis]
+			yAxis |= to_flag(y * width)
+		self._full_board = xAxis * yAxis
+		self._win_shifts = [1, width - 1, width + 1, width]
+		self._win_masks = [~yAxis, ~(xAxis | (yAxis << (width - 1))), ~(xAxis | yAxis), ~xAxis]
 		for i in range(4):
-			self.winMasks[i] &= self.winMasks[i] << self.winShifts[i]
+			self._win_masks[i] &= self._win_masks[i] << self._win_shifts[i]
 	
 	def opponent(self):
-		return self.currentPlayer ^ 1
+		return self._current_player ^ 1
+	
+	def current_player(self):
+		return self._current_player
 		
 	def occupied(self):
-		return self.playerTokens[0] | self.playerTokens[1]
+		return self._player_tokens[0] | self._player_tokens[1]
 		
-	def columnToTokenMove(self, x):
-		token = toFlag(x)
+	def column_to_token_move(self, x):
+		token = to_flag(x)
 		tokens = self.occupied()
 		while token & tokens:
-			token <<= self.width
+			token <<= self._width
 		return token
 		
-	def tokenMoves(self):
+	def token_moves(self):
 		tokens = self.occupied()
-		return ~(tokens | ~tokens << self.width) & self.fullBoard
+		return ~(tokens | ~tokens << self._width) & self._full_board
 	
-	def tokenMove(self, token):
-		self.playerTokens[self.currentPlayer] ^= token
-		self.currentPlayer = self.opponent()
+	def token_move(self, token):
+		self._player_tokens[self._current_player] ^= token
+		self._current_player = self.opponent()
 	
-	def tokenUndo(self, token):
-		self.currentPlayer = self.opponent()
-		self.playerTokens[self.currentPlayer] ^= token
+	def token_undo(self, token):
+		self._current_player = self.opponent()
+		self._player_tokens[self._current_player] ^= token
 		
-	def isBoardFull(self):
-		return self.occupied() == self.fullBoard
+	def is_board_full(self):
+		return self.occupied() == self._full_board
 		
-	def opponentWon(self):
-		tokens = self.playerTokens[self.opponent()]
+	def opponent_won(self):
+		tokens = self._player_tokens[self.opponent()]
 		for i in range(4):
 			win = tokens
-			win &= win << (2 * self.winShifts[i])
-			win &= self.winMasks[i]
-			win &= win << self.winShifts[i]
+			win &= win << (2 * self._win_shifts[i])
+			win &= self._win_masks[i]
+			win &= win << self._win_shifts[i]
 			if(win):
 				return True
 		return False
 		
-	def isGameOver(self):
-		return self.isBoardFull() or self.opponentWon()
+	def is_game_over(self):
+		return self.is_board_full() or self.opponent_won()
 		
-	def asString(self):
+	def as_string(self):
 		string = ""
-		for y in reversed(range(self.height)):
-			for x in range(self.width):
-				token = toFlag(x + y * self.width)
-				if(self.playerTokens[0] & token):
+		for y in reversed(range(self._height)):
+			for x in range(self._width):
+				token = to_flag(x + y * self._width)
+				if(self._player_tokens[0] & token):
 					string += "[X]"
-				elif(self.playerTokens[1] & token):
+				elif(self._player_tokens[1] & token):
 					string += "[O]"
 				else:
 					string += "[ ]"
@@ -77,8 +80,8 @@ class Connect4:
 		
 
 state = Connect4()
-print(state.asString())
-while not state.isGameOver():
+print(state.as_string())
+while not state.is_game_over():
 	try:
 		x = int(input('Select move column:'))
 	except ValueError:
@@ -87,7 +90,7 @@ while not state.isGameOver():
 		if(x < 0 or state.width <= x):
 			print("Invalid column")
 		else:
-			tokenMove = state.columnToTokenMove(x)
-			state.tokenMove(tokenMove)
-			print(state.asString())
+			token_move = state.column_to_token_move(x)
+			state.token_move(token_move)
+			print(state.as_string())
 print("Game over")
