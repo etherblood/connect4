45c8f8cf033f43478f36ea9a9c33afa778aaf4ee
Etherblood <philipp@jobach.de>
2019-07-05 00:34:52 +0200

    move order computed when needed

--- java/src/main/java/com/etherblood/connect4/solver/TokenSolver.java
+++ java/src/main/java/com/etherblood/connect4/solver/TokenSolver.java
@@ -11,36 +11,11 @@ public class TokenSolver extends TokenUtil {
     private static final int WIN_SCORE = 1;
     private static final int DRAW_SCORE = 0;
     private static final int LOSS_SCORE = -1;
-    private static final long[] MOVE_ORDER_DEFAULT;
-    private static final long[] MOVE_ORDER_SYMMETRICAL;
 
     public long[] ttStats = new long[6];
     public long drawLossCutoff, drawWinCutoff;
     public long totalNodes, totalNanos;
     public final SolverTable table;
-
-    static {
-        //fill columns asap to reduce branching factor by playing topmost moves first
-        MOVE_ORDER_DEFAULT = new long[HEIGHT];
-        for (int y = 0; y < HEIGHT; y++) {
-            long mask = X_AXIS << (y * UP);
-            MOVE_ORDER_DEFAULT[HEIGHT - y - 1] = mask;
-        }
-
-        if (CENTER_COLUMN != 0) {
-            //fill center column first since it keeps symmetry (low branching)
-            MOVE_ORDER_SYMMETRICAL = new long[HEIGHT + 1];
-            MOVE_ORDER_SYMMETRICAL[0] = CENTER_COLUMN;
-            for (int y = 0; y < HEIGHT; y++) {
-                MOVE_ORDER_SYMMETRICAL[y + 1] = MOVE_ORDER_DEFAULT[y] & LEFT_SIDE;//skip right side
-            }
-        } else {
-            MOVE_ORDER_SYMMETRICAL = new long[HEIGHT];
-            for (int y = 0; y < HEIGHT; y++) {
-                MOVE_ORDER_SYMMETRICAL[y] = MOVE_ORDER_DEFAULT[y] & LEFT_SIDE;//skip right side
-            }
-        }
-    }
 
     public TokenSolver(SolverTable table) {
         this.table = table;
@@ -149,8 +124,15 @@ public class TokenSolver extends TokenUtil {
         int nextEntryScore = alpha == LOSS_SCORE ? SolverTable.LOSS_SCORE : SolverTable.DRAW_LOSS_SCORE;
         try {
             boolean symmetrical = isSymmetrical(ownTokens) && isSymmetrical(opponentTokens);
-            for (long orderMask : symmetrical ? MOVE_ORDER_SYMMETRICAL : MOVE_ORDER_DEFAULT) {
-                long movesIterator = moves & orderMask;
+            for (int phase = HEIGHT - (symmetrical ? 0 : 1); phase >= 0; phase--) {
+                long movesIterator;
+                if (phase == HEIGHT) {
+                    movesIterator = moves & CENTER_COLUMN;
+                    //mirrored moves will have identical scores and can be skipped
+                    moves &= LEFT_SIDE;
+                } else {
+                    movesIterator = moves & (X_AXIS << (phase * UP));
+                }
                 while (movesIterator != 0) {
                     long move = Long.lowestOneBit(movesIterator);
                     int score = -solve(opponentTokens, move(ownTokens, move), -beta, -alpha);
