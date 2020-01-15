86c0d07c0c41f0ef52201b3a931ea72a6e54a385
etherblood <philipp@jobach.de>
2020-01-15 23:31:55 +0100

    added mask perft for tic-tac-toe to help understand how MaskPerft works

---
+++ java/src/main/java/com/etherblood/connect4/TicTacToeMaskPerft.java
@@ -0,0 +1,66 @@
+package com.etherblood.connect4;
+
+public class TicTacToeMaskPerft {
+
+    private static final int FULL_BOARD;
+
+    private static final int EMPTY_WINS;
+    private static final int WIN_MASK;
+    private static final int[] MOVE_WINS;
+
+    static {
+        int empty = 0b001_001_001_001_001_001_001_001;
+        int move0 = 0b000_001_000_000_001_000_000_001;
+        int move1 = 0b000_000_000_001_000_000_000_001;
+        int move2 = 0b001_000_001_000_000_000_000_001;
+        int move3 = 0b000_000_000_000_001_000_001_000;
+        int move4 = 0b001_001_000_001_000_000_001_000;
+        int move5 = 0b000_000_001_000_000_000_001_000;
+        int move6 = 0b001_000_000_000_001_001_000_000;
+        int move7 = 0b000_000_000_001_000_001_000_000;
+        int move8 = 0b000_001_001_000_000_001_000_000;
+        int _wins = 0b100_100_100_100_100_100_100_100;
+
+        EMPTY_WINS = empty;
+        MOVE_WINS = new int[]{move0, move1, move2, move3, move4, move5, move6, move7, move8};
+        WIN_MASK = _wins;
+
+        FULL_BOARD = 0b111_111_111;
+    }
+
+    public static void main(String[] args) {
+        //warmup
+        for (int i = 0; i < 1000; i++) {
+            perft(FULL_BOARD, EMPTY_WINS, EMPTY_WINS);
+        }
+
+        int result = -1;
+        int iterations = 10;
+        long startNanos = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            result = perft(FULL_BOARD, EMPTY_WINS, EMPTY_WINS);
+        }
+        long nanos = System.nanoTime() - startNanos;
+        System.out.println("perft: " + result);
+        System.out.println("nanos: " + nanos / iterations);
+    }
+
+    private static int perft(int moves, int ownWins, int oppWins) {
+        if (moves == 0) {
+            return 1;
+        }
+        if ((oppWins & WIN_MASK) != 0) {
+            return 0;
+        }
+        int count = 0;
+        int iterator = moves;
+        while (iterator != 0) {
+            int moveIndex = Long.numberOfTrailingZeros(iterator);
+            int move = 1 << moveIndex;
+            iterator ^= move;
+            int nextWins = ownWins + MOVE_WINS[moveIndex];
+            count += perft(moves ^ move, oppWins, nextWins);
+        }
+        return count;
+    }
+}
