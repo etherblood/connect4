5434da9370167439583367dd453a6545ecff548b
etherblood <philipp@jobach.de>
2020-11-22 18:01:58 +0100

    played around with tic-tac-toe perft

--- java/src/main/java/com/etherblood/connect4/sandbox/TicTacToeMaskPerft.java
+++ java/src/main/java/com/etherblood/connect4/sandbox/TicTacToeMaskPerft.java
@@ -31,36 +31,79 @@ public class TicTacToeMaskPerft {
     public static void main(String[] args) {
         //warmup
         for (int i = 0; i < 1000; i++) {
-            perft(FULL_BOARD, EMPTY_WINS, EMPTY_WINS);
+            iterativePerft();
         }
 
         int result = -1;
-        int iterations = 10;
+        int iterations = 100;
         long startNanos = System.nanoTime();
         for (int i = 0; i < iterations; i++) {
-            result = perft(FULL_BOARD, EMPTY_WINS, EMPTY_WINS);
+            result = iterativePerft();
         }
         long nanos = System.nanoTime() - startNanos;
         System.out.println("perft: " + result);
         System.out.println("nanos: " + nanos / iterations);
     }
 
+    private static int iterativePerft() {
+        int count = 0;
+        int[] stack = new int[3 * 34];
+        int pointer = 0;
+        stack[pointer++] = EMPTY_WINS;
+        stack[pointer++] = EMPTY_WINS;
+        stack[pointer++] = FULL_BOARD;
+
+        while (pointer != 0) {
+            int moves = stack[--pointer];
+            int ownWins = stack[--pointer];
+            int oppWins = stack[--pointer];
+            int iterator = moves;
+            while (iterator != 0) {
+                int move = Integer.lowestOneBit(iterator);
+                int nextMoves = moves ^ move;
+                int moveIndex = Long.numberOfTrailingZeros(iterator);
+                int nextWins = ownWins + MOVE_WINS[moveIndex];
+                if ((nextWins & WIN_MASK) == 0) {
+                    if (Integer.bitCount(nextMoves) == 1) {
+                        count++;
+                    } else {
+                        stack[pointer++] = nextWins;
+                        stack[pointer++] = oppWins;
+                        stack[pointer++] = nextMoves;
+                    }
+//                } else {
+//                    // with this we would count number of games instead of perft
+//                    count++;
+                }
+                iterator &= iterator - 1;
+            }
+        }
+        return count;
+    }
+
+    private static int perft() {
+        return perft(FULL_BOARD, EMPTY_WINS, EMPTY_WINS);
+    }
+
     private static int perft(int moves, int ownWins, int oppWins) {
         int count = 0;
         int iterator = moves;
         while (iterator != 0) {
-            int moveIndex = Long.numberOfTrailingZeros(iterator);
-            int move = 1 << moveIndex;
-            iterator ^= move;
+            int move = Integer.lowestOneBit(iterator);
             int nextMoves = moves ^ move;
             if (nextMoves == 0) {
                 count++;
             } else {
+                int moveIndex = Long.numberOfTrailingZeros(iterator);
                 int nextWins = ownWins + MOVE_WINS[moveIndex];
                 if ((nextWins & WIN_MASK) == 0) {
                     count += perft(nextMoves, oppWins, nextWins);
+//                } else {
+//                    // with this we would count number of games instead of perft
+//                    count++;
                 }
             }
+            iterator &= iterator - 1;
         }
         return count;
     }
